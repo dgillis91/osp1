c48bd404ec1ef2f5f6c1d3355a9546ff6fe3f435
David Gillis <dgillis91@gmail.com>
2019-08-28 22:00:33 -0500

    added functions to get permissions

--- ref/shownames.c
+++ ref/shownames.c
@@ -28,6 +28,41 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#define MODE_COUNT 9
+mode_t MODES[MODE_COUNT] = {
+    S_IRUSR, S_IWUSR, S_IXUSR,
+    S_IRGRP, S_IWGRP, S_IXGRP,
+    S_IROTH, S_IWOTH, S_IXOTH
+};
+
+#define RWX_CHAR_COUNT 3
+char RWX_CHAR[RWX_CHAR_COUNT] = {'r', 'w', 'x'};
+
+
+void file_access_string(struct stat* file_stat, char* buffer) {
+    /* Write the unix style file access to `buffer`.
+     * Assumes length of `buffer` is 11 - which includes
+     * the null terminator.
+     */
+    const char UNSET_CHAR = '-';
+    if (S_ISDIR(file_stat->st_mode)) {
+        buffer[0] = 'd';
+    } else {
+        buffer[0] = UNSET_CHAR;
+    }
+    int i;
+    for (i = 1; i < MODE_COUNT + 1; ++i) {
+        if (file_stat->st_mode & MODES[i - 1]) {
+            buffer[i] = RWX_CHAR[(i - 1) % RWX_CHAR_COUNT];
+        } else {
+            buffer[i] = UNSET_CHAR;
+        }
+    }
+    buffer[10] = '\0';
+}
 
 
 int main(int argc, char* argv[]) {
@@ -58,7 +93,18 @@ int main(int argc, char* argv[]) {
         printf("%s:%3d\n", direntp->d_name,
                direntp->d_type);
     }
-
+    
+    struct stat file_stat;
+    if (stat(argv[1], &file_stat) == -1) {
+        perror("Unable to stat");
+        return EXIT_FAILURE;
+    }
+    char perms[11];
+    file_access_string(&file_stat, perms);
+    printf("----\n");
+    printf("%s\n", perms);
+    
+    
     // Attempt to close the directory multiple times, if
     // interrupted.
     while ((closedir(dirp) == -1) && (errno == EINTR)) ;
