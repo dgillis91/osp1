96f8a25288cabbce24cd2d4c9ea76011cf1b691e
David Gillis <dgillis91@gmail.com>
2019-09-01 10:17:51 -0500

    use free_program_options

--- src/dt.c
+++ src/dt.c
@@ -3,23 +3,33 @@
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
 #include "../include/parse.h"
+#include "../include/statq.h"
+#include "../include/filedet.h"
 
 
 int main(int argc, char* argv[]) {
+    // Pointer to the file/directory we are currently examining.
+    struct dirent* direntp;
+    // Directory stream to iterate over the directory.
+    DIR* directory_stream;
+    // Program options passed through the command line
     program_options_t* options = malloc_default_program_options();
+    // validate that the malloc worked.
     if (options == NULL) {
         fprintf(stderr, "%s: Error: %s\n", argv[0], strerror(errno));
         return EXIT_FAILURE;
     }
     parse_options(argc, argv, options);
 
-    fprintf(stdout, "I: %d\n", options->space_count_indentation);
-    fprintf(stdout, "L: %d\n", options->is_follow_links);
-    fprintf(stdout, "t: %d\n", options->is_display_filetype);
-    fprintf(stdout, "p: %d\n", options->is_display_permissions);
+    printf("optind: %d\n", optind);
+    printf("opt: %s\n", argv[optind]);
 
-    free(options);
+    // Be a good neighbor.
+    free_program_options(options);
 
     return EXIT_SUCCESS;
 }
